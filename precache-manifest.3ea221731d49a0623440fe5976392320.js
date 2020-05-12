4087fd393e15c836148edcacdfa6e51bce736d4c
Myr7You <myr7you@gmail.com>
2020-05-12 11:23:17 +0800

    deploy

---
+++ precache-manifest.3ea221731d49a0623440fe5976392320.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74fa4904f995cff3771b",
+    "url": "/Myr7You/css/app.58a083bc.css"
+  },
+  {
+    "revision": "4e0e91a01f294b6e6cff",
+    "url": "/Myr7You/css/chunk-vendors.6e808e7d.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/Myr7You/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/Myr7You/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "578d774fe7294f85fcaf08475662cd99",
+    "url": "/Myr7You/img/123.578d774f.jpg"
+  },
+  {
+    "revision": "a4715d0a5ce33599e3d892e46fe2153d",
+    "url": "/Myr7You/img/IMG_0002.a4715d0a.jpg"
+  },
+  {
+    "revision": "d1bb54b34b08bf57075a484dfa83b877",
+    "url": "/Myr7You/img/IMG_3671.d1bb54b3.jpg"
+  },
+  {
+    "revision": "15ccc83c69769e9b6c9eff127dcf358e",
+    "url": "/Myr7You/index.html"
+  },
+  {
+    "revision": "74fa4904f995cff3771b",
+    "url": "/Myr7You/js/app.478017e4.js"
+  },
+  {
+    "revision": "4e0e91a01f294b6e6cff",
+    "url": "/Myr7You/js/chunk-vendors.ba178513.js"
+  },
+  {
+    "revision": "111087121521da95a8f10d8b2106ac9e",
+    "url": "/Myr7You/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Myr7You/robots.txt"
+  }
+]);
